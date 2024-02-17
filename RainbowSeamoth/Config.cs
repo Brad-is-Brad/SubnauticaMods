94b235c90ce30fe219825c39bda1f4800a0b850a
Brad-is-Brad <brad@bradisbrad.com>
2024-02-16 23:28:06 -0600

    Add RainbowSeamoth

---
+++ RainbowSeamoth/Config.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace RainbowSeamoth
+{
+    internal class Config
+    {
+        public float changeSpeed = 100f;
+        public bool changeMain = true;
+        public bool changeName = true;
+        public bool changeInterior = true;
+        public bool changeStripe1 = true;
+        public bool changeStripe2 = true;
+
+        public static Config Load()
+        {
+            try
+            {
+                string path = Path.Combine(Mod.ModPath, "config.json");
+                if (!File.Exists(path))
+                {
+                    Plugin.Logger.LogDebug($"No config file found. Creating...");
+                    Config config = new Config();
+                    config.Save();
+                    return config;
+                }
+                else
+                {
+                    Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+                    if (config == null)
+                    {
+                        throw new Exception("Could not load config.");
+                    }
+                    return config;
+                }
+            }
+            catch (Exception ex)
+            {
+                Plugin.Logger.LogDebug($"Error loading config: {ex.Message}");
+                Plugin.Logger.LogDebug($"\n{ex.StackTrace}");
+                return null;
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                string path = Path.Combine(Mod.ModPath, "config.json");
+                string contents = JsonConvert.SerializeObject(this, Formatting.Indented);
+                File.WriteAllText(path, contents);
+            }
+            catch (Exception ex)
+            {
+                Plugin.Logger.LogDebug($"Error saving config: {ex.Message}");
+                Plugin.Logger.LogDebug($"\n{ex.StackTrace}");
+            }
+        }
+    }
+}
---
+++ RainbowSeamoth/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RainbowSeamoth
+{
+    [BepInPlugin("com.bradisbrad.RainbowSeamoth", "RainbowSeamoth", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        internal static Config config;
+
+        private void Awake()
+        {
+            try
+            {
+                Logger = base.Logger;
+                var assembly = Assembly.GetExecutingAssembly();
+                var modName = ($"{assembly.GetName().Name}");
+                Logger.LogInfo($"{modName} loaded!");
+                Harmony harmony = new Harmony(modName);
+                harmony.PatchAll(assembly);
+                Logger.LogInfo($"{modName} patched!");
+
+                config = RainbowSeamoth.Config.Load();
+            }
+            catch (System.Exception e)
+            {
+                Logger.LogInfo($"{e}");
+            }
+        }
+    }
+}
